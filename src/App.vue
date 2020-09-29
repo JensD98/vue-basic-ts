c82100d2e18583d126d4ab94032d5df21a314cf4
Jens <jens@doellmann.com>
2020-09-29 21:02:23 +0200

    Spelling

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <h1>Heading</h1>
   <parent>
-    <p>Paragrpah 1</p>
+    <p>Paragraph 1</p>
     <child>
-      <p>Paragrpah 1 - child 1</p>
-      <p>Paragrpah 2 - child 2</p>
+      <p>Paragraph 1 - child 1</p>
+      <p>Paragraph 2 - child 2</p>
     </child>
   </parent>
 </template>
